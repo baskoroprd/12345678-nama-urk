cbeff4ba14cff55633600c746b473f43e688e409
Baskoro Pradito <baskorob810@gmail.com>
2024-08-10 18:19:38 +0700

    v1.2.1

--- proses.js
+++ proses.js
@@ -1,1 +1,23 @@
- 
+function calculate() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const operation = document.getElementById("operation").value;
+    let result;
+  
+    switch (operation) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+    }
+    console.log(result);
+  }
+   
